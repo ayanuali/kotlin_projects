6933e3baea1c9db3d5a7c931673081d6c7bfcd5c
Ayan Uali <ayanuali@gmail.com>
2021-08-13 18:15:18 +0600

    layout unique ids

--- DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ayanuali.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val roll_button: Button = findViewById(R.id.roll_button)
+        roll_button.setText("Lets roll")
     }
 }

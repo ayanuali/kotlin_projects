b5eee49e24be53762cb77925a5431e16d40b7f57
Ayan Uali <ayanuali@gmail.com>
2021-08-13 21:19:31 +0600

    lateinit

--- DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+//    val diceImage: ImageView? = null
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 //            Toast.makeText(this, "Button clicked" , Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -35,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+//        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
 
     }

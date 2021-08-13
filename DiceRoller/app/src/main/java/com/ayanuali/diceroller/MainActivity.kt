24d93f1db90e52a5ae5e5a761981c87649ff2211
Ayan Uali <ayanuali@gmail.com>
2021-08-13 21:03:12 +0600

    random numbers

--- DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/ayanuali/diceroller/MainActivity.kt
@@ -3,12 +3,28 @@ package com.ayanuali.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val roll_button: Button = findViewById(R.id.roll_button)
-        roll_button.setText("Lets roll")
+//        roll_button.setText("Lets roll")
+        roll_button.setOnClickListener {
+//            Toast.makeText(this, "Button clicked" , Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+//        resultText.setText("dice rolled")
+        val randonInt = Random.nextInt(1, 7)
+        resultText.text = randonInt.toString()
+
     }
 }

846a1996a8bb8d0a58e4491e9a91eaae7628ab17
Ayan Uali <ayanuali@gmail.com>
2021-08-15 22:22:42 +0600

    click listerners

--- ColorMyViews/app/src/main/java/com/ayanuali/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/ayanuali/colormyviews/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.ayanuali.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.ayanuali.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.apply {
+            val listOfViews: List<View> = listOf(
+                boxOneText,
+                boxTwoText,
+                boxThreeText,
+                boxFourText,
+                boxFiveText,
+                constraintLayout
+            )
+
+            for (i in listOfViews) {
+                i.setOnClickListener { makeColored(i) }
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }

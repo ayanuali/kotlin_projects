6cc9927dc6b795551f2007860dced49a5ffd9d19
Ayan Uali <ayanuali@gmail.com>
2021-08-20 16:08:41 +0600

    score view model wrap up

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -32,9 +33,10 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+    private lateinit var binding: ScoreFragmentBinding
     private lateinit var scoreViewModel: ScoreViewModel
     private lateinit var scoreViewModelFactory: ScoreViewModelFactory
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
@@ -42,6 +44,17 @@ class ScoreFragment : Fragment() {
         scoreViewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
         scoreViewModel =
             ViewModelProvider(this, scoreViewModelFactory).get(ScoreViewModel::class.java)
+
+        scoreViewModel.score.observe(this, Observer {
+            binding.scoreText.text = it.toString()
+        })
+
+        scoreViewModel.eventPlayAgain.observe(this, Observer {
+            if (it) {
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                scoreViewModel.onPlayAgainComplete()
+            }
+        })
 
     }
 
@@ -52,7 +65,7 @@ class ScoreFragment : Fragment() {
     ): View? {
 
         // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.score_fragment,
             container,
@@ -60,9 +73,9 @@ class ScoreFragment : Fragment() {
         )
 
         // Get args using by navArgs property delegate
-        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+//        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+
+        binding.playAgainButton.setOnClickListener { scoreViewModel.onPlayAgain() }
 
         return binding.root
     }

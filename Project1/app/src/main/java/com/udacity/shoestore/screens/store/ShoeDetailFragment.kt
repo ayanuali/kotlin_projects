15bfdded0a2f3272a3aafc7de36dc84143d75be1
Ayan Uali <ayanuali@gmail.com>
2021-08-28 23:51:08 +0600

    safe args

--- Project1/app/src/main/java/com/udacity/shoestore/screens/store/ShoeDetailFragment.kt
+++ Project1/app/src/main/java/com/udacity/shoestore/screens/store/ShoeDetailFragment.kt
@@ -6,20 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.ActivityViewModel
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 
 class ShoeDetailFragment : Fragment() {
     lateinit var binding: ShoeDetailFragmentBinding
+    lateinit var activityViewModel: ActivityViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.shoe_detail_fragment, container, false)
+        activityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
         binding.cancel.setOnClickListener {
             findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoesListFragment())
+        }
+        binding.save.setOnClickListener {
+            findNavController().navigate(
+                ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoesListFragment()
+                    .setShoeAdded(true)
+            )
         }
         return binding.root
     }
--- Project1/app/src/main/java/com/udacity/shoestore/screens/store/ShoesListFragment.kt
+++ Project1/app/src/main/java/com/udacity/shoestore/screens/store/ShoesListFragment.kt
@@ -8,11 +8,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoesListFragmentBinding
+import timber.log.Timber
 
 class ShoesListFragment : Fragment() {
     lateinit var binding: ShoesListFragmentBinding
+    private val args: ShoesListFragmentArgs by navArgs<ShoesListFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +23,13 @@ class ShoesListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.shoes_list_fragment, container, false)
+
+        val shoeAdded = args.shoeAdded
+        Timber.i("shoeAdded $shoeAdded")
+
         binding.addShoe.setOnClickListener {
             val action = ShoesListFragmentDirections.actionShoesListFragmentToShoeDetailFragment()
+
             findNavController().navigate(action)
         }
         return binding.root

9a2ddd4af7b40a7dc9941075e67296cb2a6f7a9e
Ayan Uali <ayanuali@gmail.com>
2021-09-19 00:36:31 +0600

    reporsitory class

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,18 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import com.example.android.devbyteviewer.database.VideoDatabase
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDomainModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository(private val database: VideoDatabase) {
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDomainModel())
+        }
+    }
+}

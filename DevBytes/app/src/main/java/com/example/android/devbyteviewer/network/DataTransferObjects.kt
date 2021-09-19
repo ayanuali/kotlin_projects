fcfbb82d1eefbb2eedee6d5a63d31dcef7261c41
Ayan Uali <ayanuali@gmail.com>
2021-09-19 19:10:10 +0600

    repository class

--- DevBytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -52,10 +52,22 @@ data class NetworkVideo(
     val closedCaptions: String?
 )
 
+fun NetworkVideoContainer.asDomainModel(): List<Video> {
+    return videos.map {
+        Video(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
+    }
+}
+
 /**
  * Convert Network results to database objects
  */
-fun NetworkVideoContainer.asDomainModel(): Array<DatabaseVideo> {
+fun NetworkVideoContainer.asDataBaseModel(): Array<DatabaseVideo> {
     return videos.map {
         DatabaseVideo(
             title = it.title,
